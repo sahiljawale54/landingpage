68e51ef8147d97572c7863b3b245afb2ef851aa3
sahiljawale54 <20510057.dypit@dypvp.edu.in>
2023-02-10 19:02:56 +0530

    added motion

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './Banner.css';
 import earth from '../../Images/save.png';
+import {motion} from 'framer-motion';
 
 const Banner = () => {
+  const transition = {
+    duration: 3,
+    type: "spring",
+  };
   return (
     <div className="container-fuild" id='banner'>
       <div className="row">
 
-        <div className="col-md-12 intro">
+        <motion.div 
+          initial={{ left: "-30rem"}}
+          whileInView={{ left: "2rem" }}
+          transition={transition} className="col-md-12 intro">
 
-          <div className='col-md-12 content'>
+          <div
+          className='col-md-12 content'>
             <span>Want to </span>
             <span>make Earth a</span>
             <span>better Place </span>
@@ -18,7 +27,7 @@ const Banner = () => {
 
           <button className='btn b-btn'>Join Us Now</button>
 
-        </div>
+        </motion.div>
       </div>
     </div>
   )
--- src/Components/Offer/Offer.jsx
+++ src/Components/Offer/Offer.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import kit from '../../Images/kit.png'
 import './Offer.css';
+import {motion} from 'framer-motion';
 
 const Offer = () => {
+  const transition = {
+    duration: 3,
+    type: "spring",
+  };
   return (
     <div className="container-fuild" id="offer">
       
-      <div className="o-body col-md-12">
-            <span>We present all new </span>
-            <span>    Sustainability Starter Kit</span>
-      </div>
+      <motion.div 
+      initial={{left: "-300px"}}
+      whileInView={{left :"3px"}}
+      transition={transition}
 
-      <div className="col-md-12 o-right">
+      className="of-body col-md-12">
+            <span>We present all new </span>
+            <span>Sustainability Starter Kit</span>
+      </motion.div>
+
+      <motion.div 
+      initial={{left: "350px"}}
+      whileInView={{left :"3px"}}
+      transition={transition}
+      className="col-md-12 of-right">
             <img src={kit} alt="" />
-      </div>
+      </motion.div>
         
     </div>
       
